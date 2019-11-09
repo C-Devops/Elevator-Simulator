1c869374c54bec32cb34b64109583a13a2a1e74d
Neryruiz <56979477+Neryruiz@users.noreply.github.com>
2019-11-09 14:29:23 -0500

    updated version of elevators

--- ElevatorEmulator/Elevators.h
+++ ElevatorEmulator/Elevators.h
@@ -1,27 +1,33 @@
 #include <iostream>
-#include <ctime>
+//#include <ctime>
 using namespace std;
 
-#ifndef ELEVATOREMULATOR_ELEVATORS_H
-#define ELEVATOREMULATOR_ELEVATORS_H
+#ifndef ELEVATORS_H_
+#define ELEVATORS_H_
 
 
 class Elevators {
-
-public:
-    /*
-     * Attributes
-     */
-    int maxCapacity;
+private:
+	int elevator_id;
+	int maxCapacity;
+	int current_floor;
+	bool current_direction; //false == up, true == down
+	bool door;//false == close, true == open
+	vector<string> onboard;//hold pass ids
 
 public:
     /*
      *  Constructor and Dest
      */
-    Elevators();
+    Elevators(int e, int max = 10);
     ~Elevators();
-
+    bool check_door();//check if door is open
+	void toggle_door();//open or close door
+	int get_current_floor();//return current floor
+	void next_floor();//increment or deincrement floor number
+	bool get_current_direction();//return direction
+	void toggle_direction();//change direction
 };
 
 
-#endif //ELEVATOREMULATOR_ELEVATORS_H
+#endif //!ELEVATORS_H
