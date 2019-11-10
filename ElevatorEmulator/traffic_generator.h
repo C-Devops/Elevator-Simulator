77659db9d98b172afd87302f54d8877d962f70d1
Neryruiz <56979477+Neryruiz@users.noreply.github.com>
2019-11-09 20:04:27 -0500

    updated
    updated set_probability_call

--- ElevatorEmulator/traffic_generator.h
+++ ElevatorEmulator/traffic_generator.h
@@ -15,16 +15,21 @@ using namespace std;
 
 class traffic_generator{
 private:
-    float vistor_rate;
+    /*float vistor_rate;
     float patients_rate;
     float sup_staff_rate;
     float med_staff_rate;
-    float security_staff_rate;
+    float security_staff_rate;*/
+    
+    //vistor range in a 100
     int vistor_range [2];
     int patients_range [2];
     int sup_staff_range [2];
     int med_staff_range [2];
     int security_staff_range [2];
+    
+    //vector holding value of type of elevator calls, call rand()%100 place
+    //used in set_probability_call()
     vector<int> vistor_call_rate;
     vector<int> patients_call_rate;
     vector<int> sup_staff_call_rate;
@@ -36,13 +41,14 @@ public:
     traffic_generator(vector<float> &rate, vector<string> call_rate, vector<string>weight);//string vis_call_rate, string pat_call_rate, string sup_call_rate, string med_call_rate, string security_call_rate, string sup_weight, string med_weight);
     ~traffic_generator();
 
-    void set_probabilitys();
+    //create vector with value 0(normal), 1(emer), 3(evac)
+    vector<int> set_probabilitys_call(vector<int> prop);
     
     //return random call 0(normal), 1(emergency), 2(evacuate) depending on type of passenager
     int passenger_elev_call(string type);
     
     //return passenager object
-    Passenger gen_pass();
+    Passenger * gen_pass();
 
 };
 
